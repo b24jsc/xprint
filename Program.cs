683012838bc9b0502dd0eef353389d1ee71a4399
Tuấn Kiệt <kietlequynh@gmail.com>
2026-04-17 10:06:49 +0700

    update

--- Program.cs
+++ Program.cs
@@ -67,6 +67,22 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<ReportDbContext>();
+
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Có lỗi xảy ra khi tự động cập nhật Database.");
+    }
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
     services.GetService<ReportDbContext>().InitializeDatabase();
 }
 
